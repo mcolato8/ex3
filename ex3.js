11449f466a90046b5f8d7ed46c5ace43fec48ce1
Michelle Colato <michellecolato@campus-029-147.ucdavis.edu>
2016-02-02 13:33:21 -0800

    ex3 commit

---
+++ ex3.js
@@ -0,0 +1,391 @@
+//Javascript
+
+//check that the html is loaded 
+
+window.onload=function(){
+    console.log("i am here")
+}
+
+
+
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip').hide();
+
+      $('#soulTitle').hover(function() {
+          $('#soulText').show("soulText");
+      }, function(){
+          $('#soulText').hide();
+            
+       
+      });
+       return false;
+});
+
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip').hide();
+
+      $('#eightThirty').hover(function() {
+          $('#soulText2').show("soulText2");
+      }, function(){
+          $('#soulText2').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip').hide();
+
+      $('#eightFortyfive').hover(function() {
+          $('#soulText3').show("soulText3");
+      }, function(){
+          $('#soulText3').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip').hide();
+
+      $('#nine').hover(function() {
+          $('#soulText4').show("soulText4");
+      }, function(){
+          $('#soulText4').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip').hide();
+
+      $('#nineFifteen').hover(function() {
+          $('#soulText5').show("soulText5");
+      }, function(){
+          $('#soulText5').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip').hide();
+
+      $('#nineThirty').hover(function() {
+          $('#soulText6').show("soulText6");
+      }, function(){
+          $('#soulText6').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip').hide();
+
+      $('#nineFourtyfive').hover(function() {
+          $('#soulText7').show("soulText7");
+      }, function(){
+          $('#soulText7').hide();
+            
+       
+      });
+       return false;
+});
+
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip').hide();
+
+      $('#ten').hover(function() {
+          $('#soulText8').show("soulText8");
+      }, function(){
+          $('#soulText8').hide();
+            
+       
+      });
+       return false;
+});
+
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip').hide();
+
+      $('#ten').hover(function() {
+          $('#soulText8').show("soulText8");
+      }, function(){
+          $('#soulText8').hide();
+            
+       
+      });
+       return false;
+});
+
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip2').hide();
+
+      $('#spiritTitle').hover(function() {
+          $('#spiritText').show("spiritText");
+      }, function(){
+          $('#spiritText').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip2').hide();
+
+      $('#sixFourtyfive').hover(function() {
+          $('#spiritText2').show("spiritText2");
+      }, function(){
+          $('#spiritText2').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip2').hide();
+
+      $('#eight').hover(function() {
+          $('#spiritText3').show("spiritText3");
+      }, function(){
+          $('#spiritText3').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip2').hide();
+
+      $('#eightFifteen').hover(function() {
+          $('#spiritText4').show("spiritText4");
+      }, function(){
+          $('#spiritText4').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip3').hide();
+
+      $('#bodyTitle').hover(function() {
+          $('#bodyText').show("bodyText");
+      }, function(){
+          $('#bodyText').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip3').hide();
+
+      $('#sixFifteen').hover(function() {
+          $('#bodyText2').show("bodyText2");
+      }, function(){
+          $('#bodyText2').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip3').hide();
+
+      $('#seven').hover(function() {
+          $('#bodyText3').show("bodyText3");
+      }, function(){
+          $('#bodyText3').hide();
+            
+       
+      });
+       return false;
+});
+
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip3').hide();
+
+      $('#sevenFifteen').hover(function() {
+          $('#bodyText4').show("bodyText4");
+      }, function(){
+          $('#bodyText4').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip3').hide();
+
+      $('#sevenThirty').hover(function() {
+          $('#bodyText5').show("bodyText5");
+      }, function(){
+          $('#bodyText5').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip3').hide();
+
+      $('#sevenFortyfive').hover(function() {
+          $('#bodyText6').show("bodyText6");
+      }, function(){
+          $('#bodyText6').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip3').hide();
+
+      $('#tenThirty').hover(function() {
+          $('#bodyText7').show("bodyText7");
+      }, function(){
+          $('#bodyText7').hide();
+            
+       
+      });
+       return false;
+});
+
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip4').hide();
+
+      $('#mindTitle').hover(function() {
+          $('#mindText').show("mindText");
+      }, function(){
+          $('#mindText').hide();
+            
+       
+      });
+       return false;
+});
+
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip4').hide();
+
+      $('#fiveFortyfive').hover(function() {
+          $('#mindText2').show("mindText2");
+      }, function(){
+          $('#mindText2').hide();
+            
+       
+      });
+       return false;
+});
+
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip4').hide();
+
+      $('#six').hover(function() {
+          $('#mindText3').show("mindText3");
+      }, function(){
+          $('#mindText3').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip4').hide();
+
+      $('#sixThirty').hover(function() {
+          $('#mindText4').show("mindText4");
+      }, function(){
+          $('#mindText4').hide();
+            
+       
+      });
+       return false;
+});
+
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip4').hide();
+
+      $('#tenFifteen').hover(function() {
+          $('#mindText5').show("mindText5");
+      }, function(){
+          $('#mindText5').hide();
+            
+       
+      });
+       return false;
+});
+
+$(document).ready(function() {
+      console.log("ready");
+      $('.tip4').hide();
+
+      $('#tenFortyfive').hover(function() {
+          $('#mindText6').show("mindText6");
+      }, function(){
+          $('#mindText6').hide();
+            
+       
+      });
+       return false;
+});
+
+
+
+
+
+
+
+
